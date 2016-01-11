e695c720e6490029013f7c44d8e4895d6d5cdbf6
Mihai Broatec <broatec.mihai@gmail.com>
2016-01-11 13:25:16 +0200

    modul reducere

--- Cupon/Reducere/controllers/IndexController.php
+++ Cupon/Reducere/controllers/IndexController.php
@@ -4,7 +4,7 @@
     class Cupon_Reducere_IndexController extends Mage_Core_Controller_Front_Action {
 
         public function indexAction() {
-            echo "esti in index<br>";
+
             $date = Mage::getModel('Cupon_Reducere/sales_flat_order');
 
             $collection = $date->getCollection();
@@ -19,20 +19,34 @@
             if (Mage::getSingleton('customer/session')->isLoggedIn()) {
                 $customerData = Mage::getSingleton('customer/session')->getCustomer();
                 $id_customer = $customerData->getId();
-            }
-            
-            $connection = Mage::getModel('core/resource')->getConnection('core_read');
-            $sql = 'SELECT * FROM `sales_flat_order`';
-            $orders = $connection->fetchAll($sql);
-            //print_r($orders);
+                $mail_customer = $customerData->getEmail();
 
-            foreach ($orders as $key) {
+                $connection = Mage::getModel('core/resource')->getConnection('core_read');
+                $sql = 'SELECT * FROM `sales_flat_order`';
+                $orders = $connection->fetchAll($sql);
                 
-                 if ($key[customer_id] == $id_customer) $comenzi++;
+                //se verifica numarul de comenzi
+                foreach ($orders as $key) {
+
+                    if ($key[customer_id] == $id_customer)
+                        $comenzi++;
+                }
+                
+                //daca e prima comanda
+                if ($comenzi == 1) {
+                    //Email information
+                    $admin_email = "broatec.mihai@gmail.com";
+                    $email = $mail_customer;
+                    $subject = "Reducere de 20% la urmatoarea comanda";
+                    $comment = "Ati primit o reducere de 20% la urmatoarea comanda. Codul de reducere este: BDG34F";
+
+                    //send email
+                    mail($admin_email, "$subject", $comment, "From:" . $email);
+                    echo "Email-ul a fost trimis!";
+                    
+                } else
+                    echo "<br>Nu se trimite mail cu reducere!";
             }
-            echo "Clientul cu id-ul:".$id_customer." are ".$comenzi." comenzi"; 
-            if($comenzi==1) echo "<br>Trimite mail cu reducerea";
-            else echo "<br>nu trimite mail";
         }
 
     }
